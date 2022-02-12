804cd9e984b7568337b74ab772ccfda25a771af9
astra <zorkin.roman@gmail.com>
2022-02-12 16:20:12 +0300

    Tasks of the first week. exercise 1, 2

---
+++ hello.py
@@ -0,0 +1,16 @@
+"""Tasks of the first week."""
+
+# exercise 1
+print('Привет мир!')
+
+# exercise 2
+print('Привет программист!')
+x_value = 2
+y_value = 2
+answer = x_value + y_value
+print(f'Результат действия {x_value} + {y_value} = {answer}')
+divisor = 3
+divisible = 10
+answer = divisor / divisor
+print(answer)
+
